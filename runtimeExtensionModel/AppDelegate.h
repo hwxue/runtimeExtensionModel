bb4b982affe09dcd2678f6eaa6346234b59b77cf
xhxue <2609386917@qq.com>
2018-12-07 14:10:52 +0800

    Initial Commit

---
+++ runtimeExtensionModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtimeExtensionModel
+//
+//  Created by gaucho on 2018/12/7.
+//  Copyright © 2018年 gaucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
